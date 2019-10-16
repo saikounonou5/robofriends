ffbe5c9f6415c604ac7af30372c19e31991bc589
Lauriston Nunes <elnunez5@gmail.com>
2019-10-16 10:29:59 -0400

    Updates

---
+++ precache-manifest.47cf88b35ccd20ec1c0ab7d08b3d432d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b6ab3c23f435c23c37eb35b9061b28",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "623b14152e152416031f",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "22890aba5de784637c9c",
+    "url": "/robofriends/static/css/main.7af432db.chunk.css"
+  },
+  {
+    "revision": "623b14152e152416031f",
+    "url": "/robofriends/static/js/2.51f23385.chunk.js"
+  },
+  {
+    "revision": "22890aba5de784637c9c",
+    "url": "/robofriends/static/js/main.0c2b3049.chunk.js"
+  },
+  {
+    "revision": "fa64f21900e25e9ebf3b",
+    "url": "/robofriends/static/js/runtime~main.2c7325ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
